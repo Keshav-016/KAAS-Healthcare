0ecf03bd62f4ac5183c89eee22140bd38189556b
Keshav Kumar <98182188+Keshav-016@users.noreply.github.com>
2023-07-31 11:04:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ export default function App() {
         return (
             <div>
                 {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
-                <BrowserRouter basename="/KAAS-Healthcare">
+                <BrowserRouter basename="/kaas-healthcare.vercel.app">
                     <Routes>
                         <Route path='/signup' element={<Signup />} />
                         <Route path='/' exact element={<Login />} />
@@ -49,7 +49,7 @@ export default function App() {
     else {
         return (
             <div>
-                <BrowserRouter basename="/KAAS-Healthcare">
+                <BrowserRouter basename="/kaas-healthcare.vercel.app">
                     <Routes>
                         <Route path='/signup' element={<Signup />} />
                         <Route path='/' exact element={<Login />} />
